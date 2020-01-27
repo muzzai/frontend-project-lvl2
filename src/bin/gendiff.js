462def5046716d2c7c4fa98ef5c83cd711d0dbfd
muzzai <a.watalov@gmail.com>
2020-01-27 22:22:22 +0300

    Adding format option & arguments

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,8 +3,8 @@ import program from 'commander';
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig>, <secondConfig>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
-
-console.log('whoa');
