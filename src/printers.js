d5a98de8d34c6e2e90e2e0c7dba10f827380f8f5
muzzai <a.watalov@gmail.com>
2020-01-28 18:29:40 +0300

    printer fixes

--- src/printers.js
+++ src/printers.js
@@ -1,4 +1,4 @@
 export default (diff) => {
   const diffKeys = Object.keys(diff);
-  return diffKeys.reduce((acc, key) => `${acc}\n${key}: ${diff[key]}`);
+  return diffKeys.reduce((acc, key) => `${acc}\n${key}: ${diff[key]}`, '');
 };
