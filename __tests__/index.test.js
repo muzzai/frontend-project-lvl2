d7e241921fbcb366f91614314393fccc9bfaa39a
muzzai <a.watalov@gmail.com>
2020-02-04 23:39:44 +0300

    still fixing tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,6 +8,6 @@ const exts = ['json', 'yml', 'ini'];
 test.each([exts])('test %s extension', (ext) => {
   const before = getParsedData(getFixturePath('before', ext));
   const after = getParsedData(getFixturePath('after', ext));
-  const result = getParsedData(getFixturePath('result', 'json'));
+  const result = getParsedData('__tests__/__fixtures__/result.json');
   expect(genDiff(before, after)).toEqual(result);
 })
