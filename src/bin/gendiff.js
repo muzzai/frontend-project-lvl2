ff3bce455600974bec41f3a689e8ca74dcb14c18
muzzai <a.watalov@gmail.com>
2020-02-17 15:21:06 +0300

    renaming configs

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,12 +2,16 @@
 import program from 'commander';
 import printDiff from '..';
 
+const formats = ['tree', 'plain', 'json'];
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig>, <secondConfig>')
   .option('-f, --format [type]', 'output format', 'tree')
   .action((firstConfigPath, secondConfigPath) => {
-    console.log(printDiff(firstConfigPath, secondConfigPath, program.format));
+    const output = formats.includes(program.format)
+      ? printDiff(firstConfigPath, secondConfigPath, program.format)
+      : `\n'${program.format}' is invalid output format. Valid formats are: 'tree', 'plain', 'json'.`;
+    console.log(output);
   });
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import genDiff from './buildDiff';
 import getFormatter from './formatters';
 
 
-export default (beforePath, afterPath, format = 'tree') => {
-  const dataBefore = readFromFile(beforePath);
-  const dataAfter = readFromFile(afterPath);
-  const extensionBefore = getFileExtension(beforePath);
-  const extensionAfter = getFileExtension(afterPath);
-  const before = getParser(extensionBefore)(dataBefore);
-  const after = getParser(extensionAfter)(dataAfter);
-  const diff = genDiff(before, after);
-  const makeFormatted = getFormatter(format) || (() => `\n'${format}' is invalid output format. Valid formats are: 'tree', 'plain', 'json'.`);
+export default (firstPath, secondPath, format = 'tree') => {
+  const firstData = readFromFile(firstPath);
+  const secondData = readFromFile(secondPath);
+  const firstExtension = getFileExtension(firstPath);
+  const secondExtension = getFileExtension(secondPath);
+  const parseFirstConfig = getParser(firstExtension)
+  const parseSecondConfig = getParser(secondExtension)
+  const first = parseFirstConfig(firstData);
+  const second = parseSecondConfig(secondData);
+  const diff = genDiff(first, second);
+  const makeFormatted = getFormatter(format);
   return makeFormatted(diff);
 };
