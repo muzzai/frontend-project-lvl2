569a8ff14134a35bcd6cb72eb7d54a4090eafdc6
muzzai <a.watalov@gmail.com>
2020-02-22 13:22:09 +0300

    resolving linter issues

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -27,7 +27,7 @@ const render = (diff, parentName) => {
       const makeDescription = plainDescriptions[type];
       return makeDescription(plainName, previousValue, currentValue, render, children);
     });
-  return described.filter(setting => setting.length !== 0).join('\n');
+  return described.filter((setting) => setting.length !== 0).join('\n');
 };
 
 export default render;
